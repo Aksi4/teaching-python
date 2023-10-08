7375b24d300d5fc00524bb20bdb7fcd06e9f629d
Maksym Vitruk <maksikvitruk@gmail.com>
2023-10-09 00:14:48 +0300

    Execution of task 1.4

--- lab_1/task1/server.py
+++ lab_1/task1/server.py
@@ -1,7 +1,76 @@
 import socket
 import time
+import threading 
+import sys
+
+active_clients = 0
+active_clients_lock = threading.Lock()
+
+terminate_server = False # вказівка завершення роботи сервера
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+def client_handler(client_socket):
+    global active_clients
+
+    with active_clients_lock:
+        active_clients += 1
+
+    while True:
+        data_size = client_socket.recv(1024).decode()
+
+        try:
+            data_size = int(data_size)
+        except ValueError:
+            print("Помилка: Невірний розмір даних від клієнта.")
+            client_socket.close()
+            break
+
+        data_received = b""
+        while len(data_received) < data_size:
+            data_chunk = client_socket.recv(1024)
+            if not data_chunk:
+                print(f"Виникла помилка під час отримання даних від клієнта.")
+                break
+
+            data_received += data_chunk
+
+        if len(data_received) == data_size:
+            time.sleep(5)
+
+            data_received = data_received.decode()
+            if data_received != "exit":
+                print(f"Отримано від клієнта: {data_received}")
+                current_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+                print(f"Час отримання: {current_time}")
+                response = "Дані отримано успішно."
+                client_socket.send(response.encode())
+            else:
+                print(f"Клієнт {client_address} відключився.")
+                client_socket.close()
+                break
+        else:
+            print("Помилка: Не вдалося отримати всі дані від клієнта.")
+            client_socket.close()
+
+    with active_clients_lock:
+        active_clients -= 1
+
+# функція перевірки активних клієнтів і закриття сервера
+def check_active_clients():
+    global active_clients
+    global terminate_server
+
+    while not terminate_server:
+        time.sleep(10)
+        with active_clients_lock:
+            if active_clients == 0:
+                # перевірка на закриття сокетів
+                if server_socket.fileno() != -1:
+                    print("Всі клієнти відключилися. Відключення серверу.")
+                    server_socket.close()
+                    terminate_server = True
+                break
 
 server_address = ('127.0.0.1', 12345)
 server_socket.bind(server_address)
@@ -10,40 +79,21 @@ server_socket.listen(1)
 
 print("Сервер очікує підключення...")
 
-client_socket, client_address = server_socket.accept()
-print(f"З'єднання від клієнта {client_address} встановлено.")
+# створення окремого потоку
+active_clients_thread = threading.Thread(target=check_active_clients)
+active_clients_thread.daemon = True
+active_clients_thread.start()
 
-while True:
-    data_size = client_socket.recv(1024).decode()
-    
+while not terminate_server:
     try:
-        data_size = int(data_size)
-    except ValueError:
-        print("Помилка: Невірний розмір даних від клієнта.")
-        break
-
-    data_received = b""
-    while len(data_received) < data_size:
-        data_chunk = client_socket.recv(1024)
-        if not data_chunk:
+        client_socket, client_address = server_socket.accept()
+        print(f"З'єднання від клієнта {client_address} встановлено.")
+        client_thread = threading.Thread(target=client_handler, args=(client_socket,))
+        client_thread.start()
+    except socket.error as e:
+        if terminate_server:
             break
-        data_received += data_chunk
+        else:
+            raise e
 
-    if len(data_received) == data_size:
-        data_received = data_received.decode()
-        print(f"Отримано від клієнта: {data_received}")
-        current_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-        print(f"Час отримання: {current_time}")
-
-        if data_received.lower() == "exit":
-            response = "Сервер закрив з'єднання."
-            client_socket.send(response.encode())
-            break
-
-        response = "Дані отримано успішно."
-        client_socket.send(response.encode())
-    else:
-        print("Помилка: Не вдалося отримати всі дані від клієнта.")
-
-client_socket.close()
-server_socket.close()
+sys.exit()
