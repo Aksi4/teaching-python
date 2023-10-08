d790d92510598280568ceb63ca98166643d6d6c2
Maksym Vitruk <maksikvitruk@gmail.com>
2023-10-08 17:30:49 +0300

    Performance of task 1.2

--- lab_1/task1/client.py
+++ lab_1/task1/client.py
@@ -2,11 +2,18 @@ import socket
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-server_address = ('127.0.0.1', 12345)  
+server_address = ('127.0.0.1', 12345)
 client_socket.connect(server_address)
 
 message = input("Введіть текст для відправки на сервер: ")
 
+data_size = len(message)
+
+client_socket.send(str(data_size).encode())
+
 client_socket.send(message.encode())
+
+response = client_socket.recv(1024).decode()
+print(f"Відповідь від сервера: {response}")
 
 client_socket.close()
--- lab_1/task1/server.py
+++ lab_1/task1/server.py
@@ -3,9 +3,8 @@ import time
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-server_address = ('127.0.0.1', 12345)  
+server_address = ('127.0.0.1', 12345) 
 server_socket.bind(server_address)
-
 
 server_socket.listen(1)
 
@@ -14,11 +13,39 @@ print("Сервер очікує підключення...")
 client_socket, client_address = server_socket.accept()
 print(f"З'єднання від клієнта {client_address} встановлено.")
 
-data = client_socket.recv(1024)
-print(f"Отримано від клієнта: {data.decode()}")
+# Розмір даних
+data_size = client_socket.recv(1024).decode()
 
-current_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-print(f"Час отримання: {current_time}")
+try:
+    data_size = int(data_size)
+except ValueError:
+    print("Помилка: Невірний розмір даних від клієнта.")
+    client_socket.close()
+    server_socket.close()
+    exit()
+
+
+data_received = b""  
+while len(data_received) < data_size:
+    data_chunk = client_socket.recv(1024)
+    if not data_chunk:
+        break 
+
+    data_received += data_chunk
+
+# Перевірка
+if len(data_received) == data_size:
+    time.sleep(5)
+
+    data_received = data_received.decode()
+    print(f"Отримано від клієнта: {data_received}")
+    current_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+    print(f"Час отримання: {current_time}")
+
+    response = "Дані отримано успішно."
+    client_socket.send(response.encode())
+else:
+    print("Помилка: Не вдалося отримати всі дані від клієнта.")
 
 client_socket.close()
 server_socket.close()
