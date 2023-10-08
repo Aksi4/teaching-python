61d33fb0480ba7646cafc811cf81f1b12801e306
Maksym Vitruk <maksikvitruk@gmail.com>
2023-10-08 18:08:36 +0300

    Performance of task 1.3

--- lab_1/task1/client.py
+++ lab_1/task1/client.py
@@ -5,15 +5,19 @@ client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_address = ('127.0.0.1', 12345)
 client_socket.connect(server_address)
 
-message = input("Введіть текст для відправки на сервер: ")
+while True:
+    message = input("Введіть текст для відправки на сервер (або 'exit' для завершення): ")
 
-data_size = len(message)
+    data_size = len(message)
 
-client_socket.send(str(data_size).encode())
+    client_socket.send(str(data_size).encode())
 
-client_socket.send(message.encode())
+    client_socket.send(message.encode())
 
-response = client_socket.recv(1024).decode()
-print(f"Відповідь від сервера: {response}")
+    response = client_socket.recv(1024).decode()
+    print(f"Відповідь від сервера: {response}")
+
+    if message.lower() == "exit":
+        break
 
 client_socket.close()
