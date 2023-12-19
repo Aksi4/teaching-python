ee385ad3c4abd80bd96b8acdb4f390bcb1f1dee4
Вітрук Максим <98693683+Aksi4@users.noreply.github.com>
2023-12-19 14:22:38 +0200

    lab_13 done

---
+++ lab_13/app/__init__.py
@@ -0,0 +1,48 @@
+from flask import Flask, current_app
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import CSRFProtect
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from app.views import configure_views
+
+db = SQLAlchemy()
+csrf = CSRFProtect()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
+login_manager.login_view = 'auth_bp.login'
+
+def create_app(config_class):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+
+    db.init_app(app)
+    csrf.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+
+    configure_views(app)
+
+    from app.auth.views import auth_bp
+    app.register_blueprint(auth_bp)
+
+    from app.todo.views import todo_bp
+    app.register_blueprint(todo_bp)
+
+    from app.cookies.views import cookies_bp
+    app.register_blueprint(cookies_bp)
+
+    from app.misc.views import misc_bp
+    app.register_blueprint(misc_bp)
+
+    from app.reviews.views import reviews_bp
+    app.register_blueprint(reviews_bp)
+
+    from app.post.views import post_bp
+    app.register_blueprint(post_bp)
+
+    from app.api.views import api_bp
+    app.register_blueprint(api_bp)
+
+    return app
+
+from app import views
