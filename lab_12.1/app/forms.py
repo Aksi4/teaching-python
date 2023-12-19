1eb6be74e748e642fdd84766cc5d96e5f574caee
Вітрук Максим <98693683+Aksi4@users.noreply.github.com>
2023-12-19 16:30:09 +0200

    ...

--- lab_12.1/app/forms.py
+++
@@ -1,7 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, SubmitField, PasswordField, BooleanField
-from wtforms.validators import DataRequired, Length, InputRequired, Email, ValidationError, Regexp, EqualTo
-
-
-
-
--- lab_12.1/app/post/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-
-post_bp = Blueprint("post_bp", __name__, template_folder="templates")
-
-from . import views
